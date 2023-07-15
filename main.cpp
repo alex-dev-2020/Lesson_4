82b8d79bac52b106917e99cbbb8e39f9210406f6
alex-dev-2020 <alex_dev_2020@mail.ru>
2023-07-15 23:26:57 +1000

    added score calculation & output

--- main.cpp
+++ main.cpp
@@ -11,18 +11,23 @@
 int main() {
 
 
-    double  balance;
+    double  balance, score;
 
 
     std::cout << "Pls  input balance value  \n";
     std::cin >> balance;
 
-    if (balance < 10000) {
-
+    if (balance < 1000) {
+        score = balance * 0.05;
+        std::cout <<  "Your score is :" << score  << " 5 %\n";
         balance  *= 1.05;
+        std::cout <<  "Your balance :" << balance << "\n";
     }
         else{
-            balance*= 1.03;
+        score = balance * 0.03;
+        std::cout <<  "Your score is :" << score  << " 5 %\n";
+        balance  *= 1.03;
+        std::cout <<  "Your balance :" << balance << "\n";
 
     }
 
